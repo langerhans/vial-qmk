9a2c7ab89aa5e080fac474da8b77e0ba7fa9b21e
langerhans <langerhans@users.noreply.github.com>
2023-01-02 01:19:09 +0100

    Add langerhans PCBs

---
+++ keyboards/langerhans/berceste/berceste.c
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "berceste.h"
+
+void matrix_init_kb(void) {
+    setPinOutput(GP18);
+}
+
+bool led_update_kb(led_t led_state) {
+    if (led_update_user(led_state)) {
+        writePin(GP18, led_state.caps_lock);
+    }
+    return true;
+}
