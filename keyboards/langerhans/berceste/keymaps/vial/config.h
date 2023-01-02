f3910c0e3ad1e6287ffac454ad49b6039bd2d1d2
langerhans <langerhans@users.noreply.github.com>
2023-01-02 01:19:09 +0100

    Add langerhans PCBs

---
+++ keyboards/langerhans/berceste/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA9, 0xF4, 0x2B, 0x18, 0x4E, 0xBB, 0xEC, 0x97}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
---
+++ keyboards/langerhans/riva/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x21, 0x0C, 0x15, 0x43, 0xC2, 0x74, 0x3B, 0xCA}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
