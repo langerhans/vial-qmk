0ef26e42a38dad43547bd91e1c7d3c83f2c4d064
langerhans <langerhans@users.noreply.github.com>
2023-03-09 20:08:55 +0100

    Add Unextended

---
+++ keyboards/langerhans/unextended/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF4, 0x22, 0xF6, 0x86, 0x9F, 0xC0, 0x58, 0x99}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
