3607b38264319d9f5c74a2b6eac283be1e033ba6
langerhans <langerhans@users.noreply.github.com>
2023-10-17 21:22:26 +0200

    Add Riva hotswap version

---
+++ keyboards/langerhans/riva_hs/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+#define MATRIX_ROW_PINS {GP25, GP26, GP27, GP28, GP29}
+#define MATRIX_COL_PINS {GP14, GP15, GP12, GP0, GP1, GP2, GP3, GP4, GP5, GP6, GP7, GP8, GP9, GP10, GP11}
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
---
+++ keyboards/langerhans/riva_hs/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDD, 0x33, 0x2A, 0xC4, 0x41, 0x01, 0xA0, 0x8F}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
